6b8c94eb78fb814aae76812ba2b668ec7cbea56b
aazhou1 <andrew@termfinance.io>
2024-10-30 11:45:20 -0700

    update deploy script

--- packages/evm/src/deploy/deploy_module.ts
+++ packages/evm/src/deploy/deploy_module.ts
@@ -1,16 +1,13 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const FirstAddress = "0x0000000000000000000000000000000000000001";
-
-const DAO_SAFE = "0x87eb5F76C3785936406fa93654F39b2087FD8068";
-const METAMASK = "0x325b8aB1BD08FbA28332796e6e4e979Fc3776BA9";
+const DAO_SAFE = "0x6Dc1d9c5E9150246A682F5D9C4cB816B3121803F";
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deployer } = await getNamedAccounts();
   const { deploy } = deployments;
-  const args = [METAMASK, DAO_SAFE, DAO_SAFE];
+  const args = [DAO_SAFE, DAO_SAFE, DAO_SAFE];
 
   const txCheck = await deploy("Permissions", {
     from: deployer,
